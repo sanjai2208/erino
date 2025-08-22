e8268660bd1f8d60161014b0c19163bf957a5178
sanjai2208 <sanjaiboopathi2005@gmail.com>
2025-08-22 21:57:14 +0530

    mongodb connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+
+
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 2810;
@@ -11,6 +16,16 @@ app.use(express.json());
 app.get("/", (req, res)=>{
     res.send("Server is running..");
 })
-app.listen(PORT, ()=>{
-    console.log(`server listening on port ${PORT}`);
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    const PORT = process.env.PORT || 2810;
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(" Failed to start server:", err);
+  }
+};
+
+startServer();
