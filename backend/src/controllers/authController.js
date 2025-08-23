e0fbf227c534b26821ab711ff04cb099f49bd46d
sanjai2208 <sanjaiboopathi2005@gmail.com>
2025-08-23 09:23:18 +0530

    implement user registration and login with jwt

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,63 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+};
+
+export const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    sendTokenResponse(user, 201, res);
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
+};
+
+export const login =  async (req, res, next)=> {
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({success : false, error : "please provide email and password"});
+        }
+
+        const user = await User.findOne({email}).select('+password');
+        if(!user){
+            return res.status(401).json({ success: false, error: 'Invalid credentials' });
+        }
+        const isMatch = await user.matchPassword(password);
+        if(!isMatch)
+        {
+            return res.status(401).json({ success: false, error: 'Invalid credentials' });
+        }
+        sendTokenResponse(user, 200, res);
+
+        
+    } catch (err) {
+        res.status(400).json({ success: false, error: err.message });
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,20 +2,32 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
-
-
+import authRoutes from "./routes/authRoutes.js"
 dotenv.config();
 
 
 const app = express();
 const PORT = process.env.PORT || 2810;
-app.use(cors());
+app.use(cors({
+  origin : 'http://localhost:5173',
+  credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.get("/", (req, res)=>{
-    res.send("Server is running..");
+    res.send("Lead management system api is running");
 })
+
+app.use("/api/auth" , authRoutes);
+
+
+
+
+
 const startServer = async () => {
   try {
     await connectDB();
