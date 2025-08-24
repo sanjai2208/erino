d605bf1c3214d15f5f2bfb85f318a1d31b434f3f
sanjai2208 <sanjaiboopathi2005@gmail.com>
2025-08-24 16:28:25 +0530

    final debug

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL : import.meta.env.VITE_API_BASE_URL || "https://erino-backend-j4d5.onrender.com/api" ,
+    baseURL : import.meta.env.VITE_API_BASE_URL || 'http://localhost:2810/api',
     withCredentials : true
 })
 export default apiClient;
